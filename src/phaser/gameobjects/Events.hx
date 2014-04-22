2ddd06b7da9c3b8917e1f9b71c71ee38fd2d5a6a
Sam MacPherson <xylom44@gmail.com>
2014-04-21 21:35:00 -0400

    added event dispatchers (closes #8)

--- src/phaser/gameobjects/Events.hx
+++ src/phaser/gameobjects/Events.hx
@@ -8,4 +8,22 @@ extern class Events {
 	 */
 	function new (sprite:phaser.gameobjects.Sprite);
 	
+	var onAddedToGroup:phaser.core.Signal;
+	var onRemovedFromGroup:phaser.core.Signal;
+	var onKilled:phaser.core.Signal;
+	var onRevived:phaser.core.Signal;
+	var onOutOfBounds:phaser.core.Signal;
+	var onEnterBounds:phaser.core.Signal;
+	
+	var onInputOver:phaser.core.Signal;
+	var onInputOut:phaser.core.Signal;
+	var onInputDown:phaser.core.Signal;
+	var onInputUp:phaser.core.Signal;
+	var onDragStart:phaser.core.Signal;
+	var onDragStop:phaser.core.Signal;
+	
+	var onAnimationStart:phaser.core.Signal;
+	var onAnimationComplete:phaser.core.Signal;
+	var onAnimationLoop:phaser.core.Signal;
+	
 }
