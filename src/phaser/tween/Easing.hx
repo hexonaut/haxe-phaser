79abc1c8b3d1537e9695ad3acefce5bf0230a009
Sam MacPherson <xylom44@gmail.com>
2014-05-01 13:46:09 -0400

    fixed #18

--- src/phaser/tween/Easing.hx
+++ src/phaser/tween/Easing.hx
@@ -1,7 +1,17 @@
 package phaser.tween;
 
-@:native("Phaser.Easing.Bounce")
-extern class Easing {
+@:native("Phaser.Easing.Linear")
+extern class Linear {
+	
+	/**
+	 * Ease-in.
+	 */
+	function None (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Quadratic")
+extern class Quadratic {
 	
 	/**
 	 * Ease-in.
@@ -15,6 +25,186 @@ extern class Easing {
 	
 	/**
 	 * Ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Cubic")
+extern class Cubic {
+	
+	/**
+	 * Cubic ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Cubic ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Cubic ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Quartic")
+extern class Quartic {
+	
+	/**
+	 * Quartic ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Quartic ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Quartic ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Quintic")
+extern class Quintic {
+	
+	/**
+	 * Quintic ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Quintic ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Quintic ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Sinusoidal")
+extern class Sinusoidal {
+	
+	/**
+	 * Sinusoidal ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Sinusoidal ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Sinusoidal ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Exponential")
+extern class Exponential {
+	
+	/**
+	 * Exponential ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Exponential ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Exponential ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Circular")
+extern class Circular {
+	
+	/**
+	 * Circular ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Circular ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Circular ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Elastic")
+extern class Elastic {
+	
+	/**
+	 * Elastic ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Elastic ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Elastic ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Back")
+extern class Back {
+	
+	/**
+	 * Back ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Back ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Back ease-in/out.
+	 */
+	function InOut (k:Float):Float;
+	
+}
+
+@:native("Phaser.Easing.Bounce")
+extern class Bounce {
+	
+	/**
+	 * Bounce ease-in.
+	 */
+	function In (k:Float):Float;
+	
+	/**
+	 * Bounce ease-out.
+	 */
+	function Out (k:Float):Float;
+	
+	/**
+	 * Bounce ease-in/out.
 	 */
 	function InOut (k:Float):Float;
 	
