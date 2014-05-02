52f48e795030beab36dcaa4bee40e6b858b6ba37
Sam MacPherson <xylom44@gmail.com>
2014-05-02 17:19:55 -0400

    fixed #27

--- src/phaser/tween/Easing.hx
+++ src/phaser/tween/Easing.hx
@@ -6,7 +6,7 @@ extern class Linear {
 	/**
 	 * Ease-in.
 	 */
-	function None (k:Float):Float;
+	static function None (k:Float):Float;
 	
 }
 
@@ -16,17 +16,17 @@ extern class Quadratic {
 	/**
 	 * Ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -36,17 +36,17 @@ extern class Cubic {
 	/**
 	 * Cubic ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Cubic ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Cubic ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -56,17 +56,17 @@ extern class Quartic {
 	/**
 	 * Quartic ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Quartic ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Quartic ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -76,17 +76,17 @@ extern class Quintic {
 	/**
 	 * Quintic ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Quintic ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Quintic ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -96,17 +96,17 @@ extern class Sinusoidal {
 	/**
 	 * Sinusoidal ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Sinusoidal ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Sinusoidal ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -116,17 +116,17 @@ extern class Exponential {
 	/**
 	 * Exponential ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Exponential ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Exponential ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -136,17 +136,17 @@ extern class Circular {
 	/**
 	 * Circular ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Circular ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Circular ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -156,17 +156,17 @@ extern class Elastic {
 	/**
 	 * Elastic ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Elastic ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Elastic ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -176,17 +176,17 @@ extern class Back {
 	/**
 	 * Back ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Back ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Back ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
 
@@ -196,16 +196,16 @@ extern class Bounce {
 	/**
 	 * Bounce ease-in.
 	 */
-	function In (k:Float):Float;
+	static function In (k:Float):Float;
 	
 	/**
 	 * Bounce ease-out.
 	 */
-	function Out (k:Float):Float;
+	static function Out (k:Float):Float;
 	
 	/**
 	 * Bounce ease-in/out.
 	 */
-	function InOut (k:Float):Float;
+	static function InOut (k:Float):Float;
 	
 }
